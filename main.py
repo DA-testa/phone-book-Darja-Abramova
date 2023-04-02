dedece2df22cb90f7e784d5b8a09c18807674b68
Darja-Abramova <125505024+Darja-Abramova@users.noreply.github.com>
2023-04-03 00:14:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def process_queries(queries):
         else:
             response = contacts.get(cur_query.number, 'not found')
             result.append(response)
-            return result
+    return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
